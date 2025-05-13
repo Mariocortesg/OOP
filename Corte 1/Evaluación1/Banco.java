6e365f8accdce1642f382e56ab70588cf4481771
Mariocortesg <marioalbertocortesgutierrez@gmail.com>
2025-05-13 17:16:36 -0500

    Create Banco.java

---
+++ Corte 1/Evaluación1/Banco.java
@@ -0,0 +1,26 @@
+public class Banco {
+    private double saldo;
+
+    public Banco() {
+        this.saldo = 0.0;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }
+    }
+
+    public boolean retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            return true;
+        }
+        return false;
+    }
+}
+
