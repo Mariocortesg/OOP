fbb738b7c1fc80fa9a55eb4f3c3d5c46d02c59e4
Mariocortesg <marioalbertocortesgutierrez@gmail.com>
2025-05-13 17:26:18 -0500

    Update Banco.java

--- Corte 1/Evaluación1/Banco.java
+++ Corte 1/Evaluación1/Banco.java
@@ -5,22 +5,33 @@ public class Banco {
         this.saldo = 0.0;
     }
 
+    // Getter para saldo
     public double getSaldo() {
         return saldo;
     }
 
-    public void depositar(double cantidad) {
-        if (cantidad > 0) {
-            saldo += cantidad;
+    // Setter para saldo con validación (opcional, pero mejor controlar modificaciones)
+    private void setSaldo(double saldo) {
+        if (saldo >= 0) {
+            this.saldo = saldo;
         }
     }
 
+    // Método para depositar que usa el setter
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            setSaldo(getSaldo() + cantidad);
+        }
+    }
+
+    // Método para retirar que usa el setter
     public boolean retirar(double cantidad) {
-        if (cantidad > 0 && cantidad <= saldo) {
-            saldo -= cantidad;
+        if (cantidad > 0 && cantidad <= getSaldo()) {
+            setSaldo(getSaldo() - cantidad);
             return true;
         }
         return false;
     }
 }
+
 
